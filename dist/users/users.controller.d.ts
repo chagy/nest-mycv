c12a21309f49701c739b80684296f512ef56a6da
chagy <chagy_x_@hotmail.com>
2021-12-29 15:20:03 +0700

    58

--- dist/users/users.controller.d.ts
+++ dist/users/users.controller.d.ts
@@ -1,7 +1,12 @@
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
+import { UpdateUserDto } from './dto/update-user.dto';
 export declare class UsersController {
     private usersService;
     constructor(usersService: UsersService);
     createUser(body: CreateUserDto): void;
+    findUser(id: string): Promise<import("./users.entity").User>;
+    findAllUsers(email: string): Promise<import("./users.entity").User[]>;
+    removeUser(id: string): Promise<import("./users.entity").User>;
+    updateUser(id: string, body: UpdateUserDto): Promise<import("./users.entity").User>;
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Get, Patch, Param, Query, Delete, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('auth')
 export class UsersController {
@@ -11,5 +12,27 @@ export class UsersController {
         this.usersService.create(body.email, body.password)
     }
 
+    @Get('/:id')
+    async findUser(@Param('id') id: string) {
+        const user = await this.usersService.findOne(parseInt(id))
+        if (!user) {
+            throw new NotFoundException('user not found!')
+        }
+        return user;
+    }
 
+    @Get()
+    findAllUsers(@Query('email') email: string) {
+        return this.usersService.find(email)
+    }
+
+    @Delete('/:id')
+    removeUser(@Param('id') id: string) {
+        return this.usersService.remove(parseInt(id))
+    }
+
+    @Patch('/:id')
+    updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+        return this.usersService.update(parseInt(id), body);
+    }
 }
