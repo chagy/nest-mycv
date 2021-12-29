8121530faf64e9b4b5d012ad935264d0c0bd8f87
chagy <chagy_x_@hotmail.com>
2021-12-29 13:41:51 +0700

    49

--- dist/users/users.controller.js
+++ dist/users/users.controller.js
@@ -15,9 +15,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.UsersController = void 0;
 const common_1 = require("@nestjs/common");
 const create_user_dto_1 = require("./dto/create-user.dto");
+const users_service_1 = require("./users.service");
 let UsersController = class UsersController {
+    constructor(usersService) {
+        this.usersService = usersService;
+    }
     createUser(body) {
-        console.log(body);
+        this.usersService.create(body.email, body.password);
     }
 };
 __decorate([
@@ -28,7 +32,8 @@ __decorate([
     __metadata("design:returntype", void 0)
 ], UsersController.prototype, "createUser", null);
 UsersController = __decorate([
-    (0, common_1.Controller)('auth')
+    (0, common_1.Controller)('auth'),
+    __metadata("design:paramtypes", [users_service_1.UsersService])
 ], UsersController);
 exports.UsersController = UsersController;
 //# sourceMappingURL=users.controller.js.map
